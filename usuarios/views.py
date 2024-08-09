4c33fc0d7cf808be2936690bcfef30bced865137
LucasAMiranda <lucasa.miranda@hotmail.com>
2024-08-08 21:48:33 -0300

    Cadastro de usuarios feito com sucesso

---
+++ usuarios/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem.')
+            #print(1)
+            return redirect('/usuarios/cadastro')
+
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha deve possuir pelo menos 6 caracteres.')
+            #print(2)
+            return redirect('/usuarios/cadastro')     
+            
+        users = User.objects.filter(username=username)     
+        
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com este username.')
+            #print(3)
+            return redirect('/usuarios/cadastro')
+            
+        user = User.objects.create_user(
+            username = username,
+            password = senha
+        )
+        
+        return redirect('/usuarios/logar')
